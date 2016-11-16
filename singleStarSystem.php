bf1a3e42e16aaf38dea8e141b1510f83804a3e6c
KoshiirRa <m.cubed.3@gmail.com>
2016-11-16 11:11:34 -0500

    Add files via upload
    re-engineered the output function, now places asteroid belts where they belong in the output for each belt rather than down at the bottom.  system data for planets/moons/etc now stored as an array and printed after generation.

--- singleStarSystem.php
+++ singleStarSystem.php
@@ -21,7 +21,7 @@ if ($numPlanets > 0) {
 	$gasGiants = 0; //track number of gas giants (needed for asteroid belt generation)
 	$asteroids = FALSE;  //track whether or not there are asteroid belts
 	$asteroidBelts = array(); //track number of asteroid belts (needed for asteroid belt generation)
-	
+	$planetArray = array(); //hold the text for each planet
 	//generate the planets and sort them by zone
 	$planets = genPlanetZones($starType[0], $numPlanets);
 	//var_dump($planets);
@@ -57,18 +57,19 @@ if ($numPlanets > 0) {
 				$stringHolder.="<li>Terrestrial Planet</li>";
 				break;
 			case "3":
-				$stringHolder = "Asteroid Belt ".(count($asteroidBelts)+1)."<ul><li>Asteroid Belt<strong> (SEE BOTTOM OF PAGE FOR DETAILS ON BELT)</strong></li>";
+				$stringHolder = "Asteroid Belt ".(count($asteroidBelts)+1);
 				$numBelt = count($asteroidBelts)+1;
-				array_push($asteroidBelts, $numBelt);
+				
+				array_push($asteroidBelts, $numBelt.",".($count-1));
 				$asteroids = TRUE;
 				break;
 			case "4":
 				$stringHolder.="<li>Icy Planet</li>";
 		}
-		echo $stringHolder;
 		//abort the rest of the iteration if an asteroid belt
 		if ($type == "3") {
-			echo "</ul>";
+			//$stringHolder.="</ul>";
+			array_push($planetArray, $stringHolder);
 			continue;
 		}
 
@@ -76,7 +77,7 @@ if ($numPlanets > 0) {
 		//$size[0] key: 1 = diminutive, 2 = fine, 3 = tiny, 4 = small, 5 = medium, 6 = large, 7 = huge, 8 = small gas giant, 9 = medium gas giant, 10 = huge gas giant, 11 = gargantuan gas giant, 12 = y-class brown dwarf
 		//$size[1] is the size text
 		$size = genPlanetSize($zone, $type);
-		echo "<li>".$size[1]."</li>";
+		$stringHolder.="<li>".$size[1]."</li>";
 		
 		//PLANET GRAVITY
 		//$gravity[0] key: 1 = microgravity, 2 = very low gravity, 3 = low gravity, 4 = standard gravity, 5 = high gravity, 6 = very high gravity, 7 = extreme gravity
@@ -86,55 +87,55 @@ if ($numPlanets > 0) {
 			$gravity[0] = 7;
 			$gravity[1] = "Extreme Gas Giant Gravity (way more than 4.0 G)";
 		}
-		echo "<li>".$gravity[1]."</li>";
+		$stringHolder.="<li>".$gravity[1]."</li>";
 		
 		//PLANET ATMOSPHERIC DENSITY
 		//$atmosphere[0] key: 1 = vacuum, 2 = very thin, 3 = thin, 4 = standard, 5 = dense, 6 = very dense, 7 = extremely dense
 		if ($size[0] <= 7) {
 			$atmosphere = genAtmoDense($gravity[0], $zone);
-			echo "<li>".$atmosphere[1]."</li>";
+			$stringHolder.="<li>".$atmosphere[1]."</li>";
 		}
 		
 		//PLANET ATMOSPHERIC COMPOSITION
 		//$atmosphereComposition[0] key: 1 = corrosive, 2 = poisonous, 3 = inert, 4 = breathable, 5 = breathable(tainted), 6 = variable
 		if ($atmosphere[0]!=1 && $size[0] <= 7) {
 			$atmosphereComposition = genAtmoComp($zone);
-			echo "<li>".$atmosphereComposition[1]."</li>";
+			$stringHolder.="<li>".$atmosphereComposition[1]."</li>";
 		}
 		
 		//PLANET GEOLOGY
 		//$planetGeology[0] key: 1 = very flat, 2 = flat, 3 = stndard, 4 = rugged, 5 = very rugged
 		if ($size[0] <= 7) {
 			$planetGeology = genPlanetGeo($gravity[0], $atmosphere[0]);
-			echo "<li>".$planetGeology[1]."</li>";
+			$stringHolder.="<li>".$planetGeology[1]."</li>";
 		}
 		
 		//PLANET VOLCANISM
 		//$planetVolcanism[0] key: 1 = dead, 2 = stable, 3 = active, 4 = very active, 5 = extreme
 		if ($size[0] <= 7) {
 			$planetVolcanism = genPlanetVolcanism($gravity[0], $planetGeology[0]);
-			echo "<li>".$planetVolcanism[1]."</li>";
+			$stringHolder.="<li>".$planetVolcanism[1]."</li>";
 		}
 		
 		//PLANET HYDROSPHERE
 		//$planetHydrosphere key: 0 = type, 1 = fluff text, 2 = percentage
 		if ($size[0] <= 7) {
 			$planetHydrosphere = genPlanetHydrosphere($atmosphere[0], $zone, $planetVolcanism[0], $planetGeology[0]);
-			echo "<li>".$planetHydrosphere[1]."</li>";
+			$stringHolder.="<li>".$planetHydrosphere[1]."</li>";
 		}
 		
 		//PLANET CLIMATE
 		if ($size[0] <= 7 && $atmosphere[0] != 1 && $zone == 2) {
 			$planetClimate = genPlanetClimate($atmosphere[0], $starType[0], $starType[1], $planetVolcanism[0], $planetHydrosphere[2], $size[0]);
-			echo "<li>".$planetClimate."</li>";
+			$stringHolder.="<li>".$planetClimate."</li>";
 		} elseif ($zone == 1) {
 			$planetClimate[0] = "Hot";
-			echo "<li>Hot Climate</li>";
+			$stringHolder.="<li>Hot Climate</li>";
 		} elseif ($zone == 3) {
 			$planetClimate[0] = "Cold";
-			echo "<li>Cold Climate</li>";
+			$stringHolder.="<li>Cold Climate</li>";
 		} else {
-			echo "<li>Vacuum Climate (broiling in sunlight, freezing in shadow)</li>";
+			$stringHolder.="<li>Vacuum Climate (broiling in sunlight, freezing in shadow)</li>";
 		}
 		
 		//PLANET BIOSPHERE
@@ -143,9 +144,9 @@ if ($numPlanets > 0) {
 		//biosphere complexity key: 1 = very simple, 2 = simple, 3 = basic, 4 = moderate, 5 = advanced, 6 = very advanced, 7 = native intelligence
 		if ($size[0] <= 7) {
 			$planetBiosphere = genPlanetBiosphere ($atmosphere[0], $planetHydrosphere[0], $zone, $atmosphereComposition[0], $planetVolcanism[0]);
-			echo "<li>".$planetBiosphere[2];
+			$stringHolder.="<li>".$planetBiosphere[2];
 			if ($planetBiosphere[0]!=1) {
-				echo "<ul><li>".$planetBiosphere[3];
+				$stringHolder.="<ul><li>".$planetBiosphere[3];
 				if ($planetBiosphere[1] >= 1 && $planetBiosphere[1] <= 2) {
 					$word = "basic";
 				} elseif ($planetBiosphere[1] == 3) {
@@ -154,37 +155,37 @@ if ($numPlanets > 0) {
 					$word = "complex";
 				}
 				if ($planetBiosphere[1] == 7) {
-					echo "<ul><li>".genQuirk("sentient")."</li></ul>";
+					$stringHolder.="<ul><li>".genQuirk("sentient")."</li></ul>";
 				} else {
 					$num = 1;
-					while ($num <= $moonBiosphere[1]) {
-						echo "<ul><li>Sample Lifeform: ".genClassification($word)."<ul><li>Quirks: ".genQuirk($word);
-						echo "</li></ul></li></ul>";
+					while ($num <= $planetBiosphere[1]) {
+						$stringHolder.="<ul><li>Sample Lifeform: ".genClassification($word)."<ul><li>Quirks: ".genQuirk($word);
+						$stringHolder.="</li></ul></li></ul>";
 						$num++;
 					}
-					echo "</li></ul>";
+					$stringHolder.="</li></ul>";
 				}
-				echo "</li></ul></li></ul>";
+				$stringHolder.="</li></ul></li></ul>";
 			}
-			echo "</li>";
+			$stringHolder.="</li>";
 		}
 		
 		//PLANET POPULATION
 		if ($size[0] <= 7 && $planetBiosphere[1] == 7) {
 			$planetPopulation=genPlanetPopulation($planetBiosphere[0], $planetBiosphere[1], $atmosphereComposition[0], $zone, $planetHydrosphere[0], $planetVolcanism[0]);
-			echo "<li>".$planetPopulation[1]."</li>";
+			$stringHolder.="<li>".$planetPopulation[1]."</li>";
 		}
 		
 		//PLANET TECHNOLOGY LEVEL
 		if ($size[0] <= 7 && $planetBiosphere[1] == 7) {
 			$planetTechnology=genPlanetTechnology($planetPopulation[0], $gravity[0], $atmosphereComposition[0], $atmosphere[0], $planetVolcanism[0], $planetClimate[0], $planetBiosphere[0]);
-			echo "<li>".$planetTechnology[1]."</li>";
+			$stringHolder.="<li>".$planetTechnology[1]."</li>";
 		}
 		
 		//CHECK FOR ALIEN ARTIFACTS
 		$roll = mt_rand(1,100);
 		if ($roll == 1) {
-			echo "<li>Alien Artifacts Possible</li>";
+			$stringHolder.="<li>Alien Artifacts Possible</li>";
 		}
 		
 		//MOON GENERATION (OH GODS WHY)
@@ -208,7 +209,7 @@ if ($numPlanets > 0) {
 			$numMoonlets = mt_rand(1,6)-3;
 		}
 		if ($numMoons > 0) {
-			echo "<li>".$numMoons." Moons<ul>";
+			$stringHolder.="<li>".$numMoons." Moons<ul>";
 			$currentMoon = 1;
 			while ($currentMoon <= $numMoons) {
 				if ($zone == "1") {
@@ -228,20 +229,20 @@ if ($numPlanets > 0) {
 						$moonType = 4;
 					}
 				}
-				echo "<li>Moon ".$currentMoon."<ul>";
+				$stringHolder.="<li>Moon ".$currentMoon."<ul>";
 				switch($moonType) {
 					case "2":
-						echo "<li>Terrestrial Moon</li>";
+						$stringHolder.="<li>Terrestrial Moon</li>";
 						break;
 					case "4":
-						echo "<li>Icy Moon</li>";
+						$stringHolder.="<li>Icy Moon</li>";
 						break;
 				}
 				//MOON SIZE
 				//$size[0] key: 1 = diminutive, 2 = fine, 3 = tiny, 4 = small, 5 = medium, 6 = large, 7 = huge, 8 = small gas giant, 9 = medium gas giant, 10 = huge gas giant, 11 = gargantuan gas giant, 12 = y-class brown dwarf
 				//$size[1] is the size text
 				$moonSize = genPlanetSize($zone, $moonType, TRUE, $size[0]);
-				echo "<li>".$moonSize[1]."</li>";
+				$stringHolder.="<li>".$moonSize[1]."</li>";
 				//MOON GRAVITY
 				//$gravity[0] key: 1 = microgravity, 2 = very low gravity, 3 = low gravity, 4 = standard gravity, 5 = high gravity, 6 = very high gravity, 7 = extreme gravity
 				if ($moonSize[0] <= 7) {
@@ -251,49 +252,49 @@ if ($numPlanets > 0) {
 					$moonGravity[1] = "Extreme Gas Giant Gravity (way more than 4.0 G)";
 				}
 				//var_dump($moonGravity);
-				echo "<li>".$moonGravity[1]."</li>";
+				$stringHolder.="<li>".$moonGravity[1]."</li>";
 				//MOON ATMOSPHERIC DENSITY
 				//$atmosphere[0] key: 1 = vacuum, 2 = very thin, 3 = thin, 4 = standard, 5 = dense, 6 = very dense, 7 = extremely dense
 				if ($moonSize[0] <= 7) {
 					$moonAtmosphere = genAtmoDense($moonGravity[0], $zone, TRUE);
-					echo "<li>".$moonAtmosphere[1]."</li>";
+					$stringHolder.="<li>".$moonAtmosphere[1]."</li>";
 				}
 				//MOON ATMOSPHERIC COMPOSITION
 				//$atmosphereComposition[0] key: 1 = corrosive, 2 = poisonous, 3 = inert, 4 = breathable, 5 = breathable(tainted), 6 = variable
 				if ($moonAtmosphere[0]!=1 && $moonSize[0] <= 7) {
 					$moonAtmosphereComposition = genAtmoComp($zone);
-					echo "<li>".$moonAtmosphereComposition[1]."</li>";
+					$stringHolder.="<li>".$moonAtmosphereComposition[1]."</li>";
 				}
 				//MOON GEOLOGY
 				//$planetGeology[0] key: 1 = very flat, 2 = flat, 3 = stndard, 4 = rugged, 5 = very rugged
 				if ($moonSize[0] <= 7) {
 					$moonGeology = genPlanetGeo($moonGravity[0], $moonAtmosphere[0]);
-					echo "<li>".$moonGeology[1]."</li>";
+					$stringHolder.="<li>".$moonGeology[1]."</li>";
 				}
 				//MOON VOLCANISM
 				//$planetVolcanism[0] key: 1 = dead, 2 = stable, 3 = active, 4 = very active, 5 = extreme
 				if ($moonSize[0] <= 7) {
 					$moonVolcanism = genPlanetVolcanism($moonGravity[0], $moonGeology[0]);
-					echo "<li>".$moonVolcanism[1]."</li>";
+					$stringHolder.="<li>".$moonVolcanism[1]."</li>";
 				}
 				//MOON HYDROSPHERE
 				//$planetHydrosphere key: 0 = type, 1 = fluff text, 2 = percentage
 				if ($moonSize[0] <= 7) {
 					$moonHydrosphere = genPlanetHydrosphere($moonAtmosphere[0], $zone, $moonVolcanism[0], $moonGeology[0]);
-					echo "<li>".$moonHydrosphere[1]."</li>";
+					$stringHolder.="<li>".$moonHydrosphere[1]."</li>";
 				}
 				//MOON CLIMATE
 				if ($moonSize[0] <= 7 && $moonAtmosphere[0] != 1 && $zone == 2) {
 					$moonClimate = genPlanetClimate($moonAtmosphere[0], $starType[0], $starType[1], $moonVolcanism[0], $moonHydrosphere[2], $moonSize[0]);
-					echo "<li>".$moonClimate."</li>";
+					$stringHolder.="<li>".$moonClimate."</li>";
 				} elseif ($zone == 1) {
 					$moonClimate[0] = "Hot";
-					echo "<li>Hot Climate</li>";
+					$stringHolder.="<li>Hot Climate</li>";
 				} elseif ($zone == 3) {
 					$moonClimate[0] = "Cold";
-					echo "<li>Cold Climate</li>";
+					$stringHolder.="<li>Cold Climate</li>";
 				} else {
-					echo "<li>Vacuum Climate (broiling in sunlight, freezing in shadow)</li>";
+					$stringHolder.="<li>Vacuum Climate (broiling in sunlight, freezing in shadow)</li>";
 				}
 				//MOON BIOSPHERE
 				//$planetBiosphere key: [0] = density, [1] = complexity, [2] = density fluff, [3] = complexity fluff, [4] = classification
@@ -301,9 +302,9 @@ if ($numPlanets > 0) {
 				//biosphere complexity key: 1 = very simple, 2 = simple, 3 = basic, 4 = moderate, 5 = advanced, 6 = very advanced, 7 = native intelligence
 				if ($moonSize[0] <= 7) {
 					$moonBiosphere = genPlanetBiosphere ($moonAtmosphere[0], $moonHydrosphere[0], $zone, $moonAtmosphereComposition[0], $moonVolcanism[0]);
-					echo "<li>".$moonBiosphere[2];
+					$stringHolder.="<li>".$moonBiosphere[2];
 					if ($moonBiosphere[0]!=1) {
-						echo "<ul><li>".$moonBiosphere[3];
+						$stringHolder.="<ul><li>".$moonBiosphere[3];
 						if ($planetBiosphere[1] >= 1 && $moonBiosphere[1] <= 2) {
 							$word = "basic";
 						} elseif ($moonBiosphere[1] == 3) {
@@ -312,52 +313,53 @@ if ($numPlanets > 0) {
 							$word = "complex";
 						}
 						if ($moonBiosphere[1] == 7) {
-							echo "<ul><li>".genQuirk("sentient")."</li></ul>";
+							$stringHolder.="<ul><li>".genQuirk("sentient")."</li></ul>";
 						} else {
 							$num = 1;
 							while ($num <= $moonBiosphere[1]) {
-								echo "<ul><li>Sample Lifeform: ".genClassification($word)."<ul><li>Quirks: ".genQuirk($word);
-								echo "</li></ul></li></ul>";
+								$stringHolder.="<ul><li>Sample Lifeform: ".genClassification($word)."<ul><li>Quirks: ".genQuirk($word);
+								$stringHolder.="</li></ul></li></ul>";
 								$num++;
 							}
-							echo "</li></ul>";
+							$stringHolder.="</li></ul>";
 						}
-						echo "</li></ul>";
+						$stringHolder.="</li></ul>";
 					}
-					echo "</li>";
+					$stringHolder.="</li>";
 				}
 				
 				//PLANET POPULATION
 				if ($moonSize[0] <= 7 && $moonBiosphere[1] == 7) {
 					$moonPopulation=genPlanetPopulation($moonBiosphere[0], $moonBiosphere[1], $moonAtmosphereComposition[0], $zone, $moonHydrosphere[0], $moonVolcanism[0]);
-					echo "<li>".$moonPopulation[1]."</li>";
+					$stringHolder.="<li>".$moonPopulation[1]."</li>";
 				}
 				
 				//PLANET TECHNOLOGY LEVEL
 				if ($size[0] <= 7 && $planetBiosphere[1] == 7) {
 					$moonTechnology=genPlanetTechnology($moonPopulation[0], $moonGravity[0], $moonAtmosphereComposition[0], $moonAtmosphere[0], $moonVolcanism[0], $moonClimate[0], $moonBiosphere[0]);
-					echo "<li>".$moonTechnology[1]."</li>";
+					$stringHolder.="<li>".$moonTechnology[1]."</li>";
 				}
 				
 				//CHECK FOR ALIEN ARTIFACTS
 				$roll = mt_rand(1,100);
 				if ($roll == 1) {
-					echo "<li>Alien Artifacts Possible</li>";
+					$stringHolder.="<li>Alien Artifacts Possible</li>";
 				}
-				echo "</li></ul>";
+				$stringHolder.="</li></ul>";
 				$currentMoon++;
 			}
-			echo "</ul></li>";
+			$stringHolder.="</ul></li>";
 		}
 		if ($numMoonlets > 0) {
-			echo "<li>".$numMoonlets." Moonlets</li>";
+			$stringHolder.="<li>".$numMoonlets." Moonlets</li>";
 		}
-		echo "</ul></li></ul>";
+		$stringHolder.="</ul></li></ul>";
+		array_push($planetArray, $stringHolder);
 		//end planet generation
 	}
 	//check for/generate asteroid belts
 	if ($asteroids == TRUE) {
-		echo "<hr /><strong>ASTEROID BELT DETAILS FOLLOW</strong>";
+		//echo "<hr /><strong>ASTEROID BELT DETAILS FOLLOW</strong>";
 		$beltDensity = 0;
 		$modifier = 0;
 		if ($gasGiants >= 3) {
@@ -366,10 +368,15 @@ if ($numPlanets > 0) {
 			$modifier+=2;
 		}
 		foreach ($asteroidBelts as $belt) {
-			echo "<ul><li>Asteroid Belt ".$belt."<ul>";
-			echo genRoids($modifier);
-			echo "</ul></li></ul>";
+			$hold = explode(",", $belt);
+			$stringHolder="<ul>";
+			$stringHolder.=genRoids($modifier);
+			$stringHolder.="</ul></li></ul>";
+			$planetArray[$hold[1]].=$stringHolder;
 		}
+	}
+	foreach ($planetArray as $text) {
+		echo $text;
 	}
 	//check for/generate/display other system features
 	$features = genSystemFeatures();
